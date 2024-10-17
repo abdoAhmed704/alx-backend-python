157692edc39f08559d3692ef1aa09a4eca6c5851
= <abdoahmedsayed704@gmail.com>
2024-10-17 18:21:38 +0300

    Done...

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Delay many times"""
+
+from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Delay many times"""
+
+    delays = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delays.append(await task)
+    return delays
